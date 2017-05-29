9c8599da7e0ac8e8123fde756833eb7eb0033835
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-28 22:47:36 -0500

    refactored parse. still need to account for Q as abbrevation for quarter. Solution is probably to cut Q&A section

--- begin.py
+++ begin.py
@@ -5,58 +5,27 @@ import io
 import re
 import glob
 
-files = glob.glob('AAPL/*.pdf')
+files = glob.glob('Microsoft.pdf')
+file_count = 0
 for file in files:
     text = process(file).decode()
     txt = io.StringIO(text)
     lines = txt.readlines()
-    if "Q\n" in lines:
-        question_indices = [i for i, question in enumerate(lines) if question == "Q\n"]
-        answer_indices = [i for i, answer in enumerate(lines) if answer == 'A\n']
-        question_answer_indices = question_indices + answer_indices
-        question_answer_indices.sort(key=int)
-        for index, value in enumerate(question_answer_indices):
-            print('\n\n')
-            print('************************')
-            try:
-                print(lines[question_answer_indices[index]:question_answer_indices[index+1]])
-            except IndexError:
-                print(lines[question_answer_indices[index]:])
-                print('#######################################################')
-                print('NEXT FILE')
-                print('#######################################################')
-                pass
-    elif re.search('<Q', str(lines)):
-        question_indices = [i for i, question in enumerate(lines) if ("<Q") in question]
-        answer_indices = [i for i, question in enumerate(lines) if ("<A") in question]
-        question_answer_indices = question_indices + answer_indices
-        question_answer_indices.sort(key=int)
-        for index, value in enumerate(question_answer_indices):
-            print('\n\n')
-            print('************************')
-            try:
-                print(lines[question_answer_indices[index]:question_answer_indices[index+1]])
-            except IndexError:
-                print(lines[question_answer_indices[index]:])
-                print('#######################################################')
-                print('NEXT FILE')
-                print('#######################################################')
-
-                pass
-    elif re.search('Question[:/-]', str(lines)):
-        question_indices = [index for index, question in enumerate(lines) if re.search("Question[:\-]", question)]
-        answer_indices = [index for index, question in enumerate(lines) if re.search("Answer[:\-]", question)]
-        question_answer_indices = question_indices + answer_indices
-        question_answer_indices.sort(key=int)
-        for index, value in enumerate(question_answer_indices):
-            print('\n\n')
-            print('************************')
-            try:
-                print(lines[question_answer_indices[index]:question_answer_indices[index+1]])
-            except IndexError:
-                print(lines[question_answer_indices[index]:])
-                print('#######################################################')
-                print('NEXT FILE')
-                print('#######################################################')
-
-                pass
+    question_indices = [index for index, question in enumerate(lines) if re.search('((<Q)|(Q\\n))|(Question[:-])', question)]
+    answer_indices = [index for index, answer in enumerate(lines) if re.search('((<A)|(A\\n))|(Answer[:-])', answer)]
+    question_answer_indices = question_indices + answer_indices
+    question_answer_indices.sort(key=int)
+    for index, value in enumerate(question_answer_indices):
+        print('\n\n')
+        print('************************')
+        try:
+            print(lines[question_answer_indices[index]:question_answer_indices[index+1]])
+        except IndexError:
+            print(lines[question_answer_indices[index]:])
+            file_count += 1
+            print('#######################################################')
+            print('NEXT FILE')
+            print('#######################################################')
+            pass
+print("file_count: ", file_count)
+print("question_answer_indices length: ", len(question_answer_indices))
