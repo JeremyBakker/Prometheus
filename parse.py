142550cea182450d4ac5d7e6a1a4e1f876ec07da
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-30 23:03:08 -0500

    basic parsing pattern works with AAPL

--- parse.py
+++ parse.py
@@ -5,7 +5,9 @@ import io
 import re
 import glob
 
-files = glob.glob('Oracle/*.pdf')
+# Find all the .pdf files
+files = glob.glob('AAPL/*.pdf')
+
 for file in files:
     # Convert the text file from pdf and transform it into an iterable list
     text = process(file).decode()
@@ -13,40 +15,61 @@ for file in files:
     lines = txt.readlines()
     
     # Find where the QUESTION AND ANSWER SECTION begins and ends, then slice it
-    # out
+    # out.
     question_and_answer_section_index = [index for index, item in enumerate(
         lines) if re.search('QUESTION AND ANSWER SECTION\\n', item)]
-    disclaimer_index = [index for index, item in enumerate(lines) if re.search('Disclaimer', item)]
+    disclaimer_index = [index for index, item in enumerate(lines) if re.search(
+        'Disclaimer', item)]
     question_and_answer_section = lines[question_and_answer_section_index[0]:
         disclaimer_index[0]]
     
-    if "Q\n" in lines:
-        pass
-        # Work with files produced from Q3 2011 until the present
-        # The transcript service marked each question with a capital Q and a 
-        # new line
-        # with open("One.txt", "a+") as write_file:
-        #     write_file.write(''.join(lines))
-        # question_indices = [i for i, question in enumerate(lines) if question == "Q\n"]
-        # answer_indices = [i for i, answer in enumerate(lines) if answer == 'A\n']
-        # question_answer_indices = question_indices + answer_indices
-        # question_answer_indices.sort(key=int)
-        # for index, value in enumerate(question_answer_indices):
-        #     print('\n\n')
-        #     print('************************')
-        #     try:
-        #         print(lines[question_answer_indices[index]:question_answer_indices[index+1]])
-        #     except IndexError:
-        #         print(lines[question_answer_indices[index]:])
-        #         print('#######################################################')
-        #         print('NEXT FILE')
-        #         print('#######################################################')
-        #         pass
+    # Work with files produced from Q3 2011 until the present. The transcript 
+    # service marked each question in these files with a capital Q and a new 
+    # line.
+    if "Q\n" in question_and_answer_section:
+        # Find the dots that separate each question/answer block in the pdf and 
+        # remove them
+        ellipsis_indices = [index for index, ellipsis in enumerate(
+            question_and_answer_section) if re.search('.\.\.\.\.\.\.\.\.\.\.+', 
+            ellipsis)]
+        for index in sorted(ellipsis_indices, reverse=True):
+            del question_and_answer_section[index]
+
+        # Find the copyright text at the end of each page and remove it
+        copyright_indices = [index for index, copyright_text in enumerate(
+            question_and_answer_section) if re.search("|".join(['1-877', 
+                '\d{1,2}\\n', 'Copyright', 'LLC', '\\x0c', 
+                'Q[0-9] [0-9]{4} Earnings Call', 'Corrected Transcript']), 
+                copyright_text)]
+        for index in sorted(copyright_indices, reverse=True):
+            del question_and_answer_section[index]
+
+        # Find where each question and answer begin in what remains of the 
+        # document
+        question_indices = [index-3 for index, question in enumerate(
+            question_and_answer_section) if question == "Q\n"]
+        answer_indices = [index-3 for index, answer in enumerate(
+            question_and_answer_section) if answer == 'A\n']
+        question_answer_indices = question_indices + answer_indices
+        question_answer_indices.sort(key=int)
+        question_answer_list = []
+        for index, value in enumerate(question_answer_indices):
+            try:
+                question_answer = question_and_answer_section[
+                    question_answer_indices[index]: 
+                    question_answer_indices[index + 1]]
+            except IndexError:
+                question_answer = question_and_answer_section[
+                question_answer_indices[index]:]
+            question_answer_list.append(question_answer)
+        with open("One.txt", "a+") as write_file:
+            write_file.write(str(question_answer_list))
+
+    # Work with files from Q1 2005 through Q2 2011 as available. The transcript
+    # service marked each question in these files with an angle bracket and a 
+    # capital Q.
     elif re.search('<Q', str(lines)):
         pass
-        # Work with files from Q1 2005 through Q2 2011 as available
-        # The transcript service marked each question with an angle bracket and
-        # a capital Q
         # with open("Two.txt", "a+") as write_file:
         #     write_file.write(''.join(lines))
         # question_indices = [i for i, question in enumerate(lines) if ("<Q") in question]
