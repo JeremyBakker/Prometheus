1e57ce518b4a76fb3d9e69aee2f81fdcaf81ae10
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-05-31 22:30:21 -0500

    expanded db and removed standalone line breaks when parsing

--- build_db.py
+++ build_db.py
@@ -2,11 +2,19 @@ import sqlite3
 
 with sqlite3.connect('db.sqlite3') as conn:
     c = conn.cursor()
-    sql_customer =      ''' CREATE TABLE Answer(
+    sql_answer =        ''' CREATE TABLE Answer(
                             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                             name            VARCHAR(20) NOT NULL,
                             position        VARCHAR(20) NOT NULL,
                             answer          VARCHAR(100) NOT NULL, 
                             date_of_call    VARCHAR(20) NOT NULL)
                         '''
-    c.execute(sql_customer)
+    sql_question =      ''' CREATE TABLE Question(
+                            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                            name            VARCHAR(20) NOT NULL,
+                            position        VARCHAR(20) NOT NULL,
+                            question        VARCHAR(100) NOT NULL, 
+                            date_of_call    VARCHAR(20) NOT NULL)
+                        '''
+    c.execute(sql_answer)
+    c.execute(sql_question)
--- parse.py
+++ parse.py
@@ -10,11 +10,14 @@ import sqlite3
 files = glob.glob('AAPL/*.pdf')
 
 for file in files:
-    # Convert the text file from pdf and transform it into an iterable list
+    # Convert the text file from pdf and transform it into an iterable list.
     text = process(file).decode()
     txt = io.StringIO(text)
     lines = txt.readlines()
     
+    # Grab the transcript date from the file name for the database entry below.
+    transcript_date = str(file[-13:-4])
+
     # Find where the QUESTION AND ANSWER SECTION begins and ends, then slice it
     # out.
     question_and_answer_section_index = [index for index, item in enumerate(
@@ -39,26 +42,34 @@ for file in files:
         # Find the copyright text at the end of each page and remove it
         copyright_indices = [index for index, copyright_text in enumerate(
             question_and_answer_section) if re.search("|".join(['1-877', 
-                '\d{1,2}\\n', 'Copyright', 'LLC', '\\x0c', 
+                '\d{1,2}\\n', 'Copyright', '^LLC', '\\x0c', 
                 'Q[0-9] [0-9]{4} Earnings Call', 'Corrected Transcript']), 
                 copyright_text)]
         for index in sorted(copyright_indices, reverse=True):
             del question_and_answer_section[index]
 
+        # Remove line breaks
+        new_line = "\n"
+        while new_line in question_and_answer_section: \
+            question_and_answer_section.remove(new_line) 
+        
+        # with open('transcript_no_new.txt', 'a+') as write_file:
+        #     write_file.write(str(question_and_answer_section))
+
         # Find where each question and answer begins in what remains of the 
         # document
-        question_indices = [index-3 for index, question in enumerate(
+        question_indices = [index-2 for index, question in enumerate(
             question_and_answer_section) if question == "Q\n"]
-        answer_indices = [index-3 for index, answer in enumerate(
+        answer_indices = [index-2 for index, answer in enumerate(
             question_and_answer_section) if answer == 'A\n']
         question_answer_indices = question_indices + answer_indices
         question_answer_indices.sort(key=int)
-        
-        # Loop through the question and answer section to grab the questions, 
-        # answers, and conversation partners. Put them in a list.
-        question_answer_list = []
+
+        # Lists to be removed. Here for debugging.
         question_list = []
         answer_list = []
+        # Loop through the question and answer section to grab the questions, 
+        # answers, and conversation partners. Add them to the database.
         for index, value in enumerate(question_answer_indices):
             try:
                 question_answer = question_and_answer_section[
@@ -67,29 +78,28 @@ for file in files:
             except IndexError:
                 question_answer = question_and_answer_section[
                 question_answer_indices[index]:]
+            print('question_answer', question_answer)
+            name = question_answer[0]
+            position_and_company = question_answer[1]
+            question_or_answer = ''.join(question_answer[3:]).replace('\n', '')
             if re.search(r'Q\\n', str(question_answer)):
                 question_list.append(question_answer)
-                print("question_answer[0]", type(question_answer[0]))
-                print("question_answer[1]", type(question_answer[1]))
-                print("question_answer[5:]", type(question_answer[5:]))
-                question = ''.join(question_answer[5:]).replace('\n', '')
-                print("question", type(question))
                 with sqlite3.connect('db.sqlite3') as conn:
                     c = conn.cursor()
-                    c.execute('''INSERT INTO ANSWER VALUES(NULL, ?, ?, ?, ?)''', 
-                        (question_answer[0], question_answer[1], 
-                        question, '2017-05-31'))
+                    c.execute('''INSERT INTO QUESTION VALUES(NULL, ?, ?, ?, ?
+                        )''', (name, position_and_company, question_or_answer, 
+                        transcript_date))
             else:
                 answer_list.append(question_answer)
+                with sqlite3.connect('db.sqlite3') as conn:
+                    c = conn.cursor()
+                    c.execute('''INSERT INTO ANSWER VALUES(NULL, ?, ?, ?, ?
+                        )''', (name, position_and_company, question_or_answer, 
+                        transcript_date))
         with open('question.txt', 'a+') as write_file:
             write_file.write(str(question_list))
         with open('answer.txt', 'a+') as write_file:
             write_file.write(str(answer_list))
-
-            print("question_answer", question_answer)
-            question_answer_list.append(question_answer)
-        # with open("One.txt", "a+") as write_file:
-        #     write_file.write(str(question_answer_list))
 
     # Work with files from Q1 2005 through Q2 2011 as available. The transcript
     # service marked each question in these files with an angle bracket and a 
