b25fd7b55f9466a7e00752bbe9b16fb45e904ed0
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-01 23:16:00 -0500

    parse 2005-2011 pdfs works

--- build_db.py
+++ build_db.py
@@ -2,19 +2,12 @@ import sqlite3
 
 with sqlite3.connect('db.sqlite3') as conn:
     c = conn.cursor()
-    sql_answer =        ''' CREATE TABLE Answer(
+    sql_question_answer =        ''' CREATE TABLE Transcripts(
                             id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                            name            VARCHAR(20) NOT NULL,
-                            position        VARCHAR(20) NOT NULL,
-                            answer          VARCHAR(100) NOT NULL, 
-                            date_of_call    VARCHAR(20) NOT NULL)
+                            name                    VARCHAR(20) NOT NULL,
+                            position                VARCHAR(20) NOT NULL,
+                            question_answer_text    VARCHAR(100) NOT NULL,
+                            question                INTEGER NOT NULL, 
+                            date_of_call            VARCHAR(20) NOT NULL)
                         '''
-    sql_question =      ''' CREATE TABLE Question(
-                            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-                            name            VARCHAR(20) NOT NULL,
-                            position        VARCHAR(20) NOT NULL,
-                            question        VARCHAR(100) NOT NULL, 
-                            date_of_call    VARCHAR(20) NOT NULL)
-                        '''
-    c.execute(sql_answer)
-    c.execute(sql_question)
+    c.execute(sql_question_answer)
--- parse.py
+++ parse.py
@@ -7,9 +7,10 @@ import glob
 import sqlite3
 
 # Find all the .pdf files
-files = glob.glob('AAPL/*.pdf')
+files = glob.glob('*/*.pdf')
 
 for file in files:
+    print(file)
     # Convert the text file from pdf and transform it into an iterable list.
     text = process(file).decode()
     txt = io.StringIO(text)
@@ -21,29 +22,103 @@ for file in files:
     # Find where the QUESTION AND ANSWER SECTION begins and ends, then slice it
     # out.
     question_and_answer_section_index = [index for index, item in enumerate(
-        lines) if re.search('QUESTION AND ANSWER SECTION\\n', item)]
+        lines) if re.search('Q ?UESTION ?-?AND ?-?ANSWER ?-?SECTION\\n', item)]
     disclaimer_index = [index for index, item in enumerate(lines) if re.search(
         'Disclaimer', item)]
-    question_and_answer_section = lines[question_and_answer_section_index[0]:
-        disclaimer_index[0]]
-    
-    # Work with files produced from Q3 2011 until the present. The transcript 
-    # service marked each question in these files with a capital Q and a new 
-    # line.
-    if "Q\n" in question_and_answer_section:
-        # Find the dots that separate each question/answer block in the file
-        # and remove them
-        ellipsis_indices = [index for index, ellipsis in enumerate(
-            question_and_answer_section) if re.search('.\.\.\.\.\.\.\.\.\.\.+', 
-            ellipsis)]
-        for index in sorted(ellipsis_indices, reverse=True):
-            del question_and_answer_section[index]
+    try:
+        question_and_answer_section = lines[question_and_answer_section_index[0]:
+            disclaimer_index[0]]
+    except IndexError:
+        question_and_answer_section = lines[407:
+            disclaimer_index[0]]
 
+    # # Work with files produced from Q3 2011 until the present. The transcript 
+    # # service marked each question in these files with a capital Q and a new 
+    # # line.
+    if "Q\n" in question_and_answer_section:
+        pass
+    #     # Find the dots that separate each question/answer block in the file
+    #     # and remove them
+    #     ellipsis_indices = [index for index, ellipsis in enumerate(
+    #         question_and_answer_section) if re.search('.\.\.\.\.\.\.\.\.\.\.+', 
+    #         ellipsis)]
+    #     for index in sorted(ellipsis_indices, reverse=True):
+    #         del question_and_answer_section[index]
+
+    #     # Find the copyright text at the end of each page and remove it
+    #     copyright_indices = [index for index, copyright_text in enumerate(
+    #         question_and_answer_section) if re.search("|".join(['1-877', 
+    #             '\d{1,2}\\n', 'Copyright', '^LLC', '\\x0c', 
+    #             'Q[0-9] [0-9]{4} Earnings Call', 'Corrected Transcript']), 
+    #             copyright_text)]
+    #     for index in sorted(copyright_indices, reverse=True):
+    #         del question_and_answer_section[index]
+
+    #     # Remove line breaks
+    #     new_line = "\n"
+    #     while new_line in question_and_answer_section: \
+    #         question_and_answer_section.remove(new_line) 
+        
+    #     # with open('transcript_no_new.txt', 'a+') as write_file:
+    #     #     write_file.write(str(question_and_answer_section))
+
+        # # Find where each question and answer begins in what remains of the 
+        # # document
+        # question_indices = [index-2 for index, question in enumerate(
+        #     question_and_answer_section) if question == "Q\n"]
+        # answer_indices = [index-2 for index, answer in enumerate(
+        #     question_and_answer_section) if answer == 'A\n']
+        # question_answer_indices = question_indices + answer_indices
+        # question_answer_indices.sort(key=int)
+
+    #     # Lists to be removed. Here for debugging.
+    #     question_list = []
+    #     answer_list = []
+    #     # Loop through the question and answer section to grab the questions, 
+    #     # answers, and conversation partners. Add them to the database.
+    #     for index, value in enumerate(question_answer_indices):
+    #         try:
+    #             question_answer = question_and_answer_section[
+    #                 question_answer_indices[index]: 
+    #                 question_answer_indices[index + 1]]
+    #         except IndexError:
+    #             question_answer = question_and_answer_section[
+    #             question_answer_indices[index]:]
+    #         name = question_answer[0]
+    #         position_and_company = question_answer[1]
+    #         question_or_answer_text = ''.join(question_answer[3:]).replace(
+    #             '\n', '')
+    #         if re.search(r'Q\\n', str(question_answer)):
+    #             question = True
+    #             question_list.append(question_answer)
+    #         else:
+    #             question = False
+    #             answer_list.append(question_answer)
+    #         with sqlite3.connect('db.sqlite3') as conn:
+    #             c = conn.cursor()
+    #             c.execute('''INSERT INTO TRANSCRIPTS VALUES(
+    #                 NULL, ?, ?, ?, ?, ?)''', (
+    #                     name, 
+    #                     position_and_company, 
+    #                     question_or_answer_text, 
+    #                     question,  
+    #                     transcript_date)
+    #             )
+    #     with open('question.txt', 'a+') as write_file:
+    #         write_file.write(str(question_list))
+    #     with open('answer.txt', 'a+') as write_file:
+    #         write_file.write(str(answer_list))
+
+    # Work with files from Q1 2005 through Q2 2011 as available. The transcript
+    # service marked each question in these files with an angle bracket and a 
+    # capital Q.
+    
+    elif re.search('<Q', str(question_and_answer_section)):
         # Find the copyright text at the end of each page and remove it
         copyright_indices = [index for index, copyright_text in enumerate(
-            question_and_answer_section) if re.search("|".join(['1-877', 
-                '\d{1,2}\\n', 'Copyright', '^LLC', '\\x0c', 
-                'Q[0-9] [0-9]{4} Earnings Call', 'Corrected Transcript']), 
+            question_and_answer_section) if re.search("|".join(['w ?w ?w ?', 
+                '\d{1,2}\\n', 'Company', '^LLC', '\\x0c', 'corrected transcript' 
+                'Q[0-9] [0-9]{4} Earnings Call', 'Ticker', 'Event Type', 'Date', 'AAPL', '--']), 
                 copyright_text)]
         for index in sorted(copyright_indices, reverse=True):
             del question_and_answer_section[index]
@@ -51,20 +126,17 @@ for file in files:
         # Remove line breaks
         new_line = "\n"
         while new_line in question_and_answer_section: \
-            question_and_answer_section.remove(new_line) 
-        
-        # with open('transcript_no_new.txt', 'a+') as write_file:
-        #     write_file.write(str(question_and_answer_section))
+            question_and_answer_section.remove(new_line)
 
         # Find where each question and answer begins in what remains of the 
         # document
-        question_indices = [index-2 for index, question in enumerate(
-            question_and_answer_section) if question == "Q\n"]
-        answer_indices = [index-2 for index, answer in enumerate(
-            question_and_answer_section) if answer == 'A\n']
+        question_indices = [index for index, question in enumerate(
+            question_and_answer_section) if re.search('<Q', question)]
+        answer_indices = [index for index, answer in enumerate(
+            question_and_answer_section) if re.search('<A', answer)]
         question_answer_indices = question_indices + answer_indices
         question_answer_indices.sort(key=int)
-
+        
         # Lists to be removed. Here for debugging.
         question_list = []
         answer_list = []
@@ -78,48 +150,31 @@ for file in files:
             except IndexError:
                 question_answer = question_and_answer_section[
                 question_answer_indices[index]:]
-            print('question_answer', question_answer)
-            name = question_answer[0]
-            position_and_company = question_answer[1]
-            question_or_answer = ''.join(question_answer[3:]).replace('\n', '')
-            if re.search(r'Q\\n', str(question_answer)):
-                question_list.append(question_answer)
-                with sqlite3.connect('db.sqlite3') as conn:
-                    c = conn.cursor()
-                    c.execute('''INSERT INTO QUESTION VALUES(NULL, ?, ?, ?, ?
-                        )''', (name, position_and_company, question_or_answer, 
-                        transcript_date))
+            colon_index = [index for index, value in enumerate(
+                question_answer[0]) if re.search(r':', value)]
+            name = question_answer[0][5:colon_index[0]-1]
+            position_and_company = False
+            question_or_answer_text = ''.join(question_answer)[colon_index[0]+1:
+                ].replace('\n', ' ')
+            if re.search(r'<Q', str(question_answer)):
+                question = True
+                question_list.append(question_or_answer_text)
             else:
-                answer_list.append(question_answer)
-                with sqlite3.connect('db.sqlite3') as conn:
-                    c = conn.cursor()
-                    c.execute('''INSERT INTO ANSWER VALUES(NULL, ?, ?, ?, ?
-                        )''', (name, position_and_company, question_or_answer, 
-                        transcript_date))
-        with open('question.txt', 'a+') as write_file:
-            write_file.write(str(question_list))
-        with open('answer.txt', 'a+') as write_file:
-            write_file.write(str(answer_list))
+                question = False
+                answer_list.append(question_or_answer_text)
+            with sqlite3.connect('db.sqlite3') as conn:
+                c = conn.cursor()
+                c.execute('''INSERT INTO TRANSCRIPTS VALUES(
+                    NULL, ?, ?, ?, ?, ?)''', (
+                        name, 
+                        position_and_company, 
+                        question_or_answer_text, 
+                        question,  
+                        transcript_date)
+                )
+        # with open('question.txt', 'a+') as write_file:
+        #     write_file.write(str(question_list))
+        # with open('answer.txt', 'a+') as write_file:
+        #     write_file.write(str(answer_list))
 
-    # Work with files from Q1 2005 through Q2 2011 as available. The transcript
-    # service marked each question in these files with an angle bracket and a 
-    # capital Q.
-    elif re.search('<Q', str(lines)):
-        pass
-        # with open("Two.txt", "a+") as write_file:
-        #     write_file.write(''.join(lines))
-        # question_indices = [i for i, question in enumerate(lines) if ("<Q") in question]
-        # answer_indices = [i for i, question in enumerate(lines) if ("<A") in question]
-        # question_answer_indices = question_indices + answer_indices
-        # question_answer_indices.sort(key=int)
-        # for index, value in enumerate(question_answer_indices):
-        #     print('\n\n')
-        #     print('************************')
-        #     try:
-        #         print(lines[question_answer_indices[index]:question_answer_indices[index+1]])
-        #     except IndexError:
-        #         print(lines[question_answer_indices[index]:])
-        #         print('#######################################################')
-        #         print('NEXT FILE')
-        #         print('#######################################################')
-        #         pass
+        
