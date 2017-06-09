3df1e342f295593366d9e732e1d164ff7c552e17
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-08 23:57:57 -0500

    index view can render data from db

--- Prometheus/urls.py
+++ Prometheus/urls.py
@@ -8,7 +8,7 @@ router.register(r'transcript', views.TranscriptViewSet)
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include(router.urls)),
+    # url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^$', views.index, name='index'),
 ]
--- Prometheus/views/index_view.py
+++ Prometheus/views/index_view.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from Prometheus.models.transcript_model import Transcripts
 
 def index(request):
     
     template_name = 'index.html'
     context = {}
+
+    transcripts = Transcripts.objects.get(pk=1)
+    context = {"transcripts": transcripts}
 
     return render(request, template_name, context)
