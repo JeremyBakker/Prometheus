6289c3d5aeeb5ac9885cadd382d81492e44a7389
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-09 14:07:59 -0500

    fixed x-axis label positioning

--- Prometheus/static/djia.js
+++ Prometheus/static/djia.js
@@ -1,4 +1,4 @@
-var margin = {top: 30, right: 65, bottom: 50, left: 50},
+var margin = {top: 30, right: 65, bottom: 70, left: 50},
     width = 860 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
@@ -75,7 +75,7 @@ svg.selectAll("dot")
 svg.append("text")             
     .attr("transform",
         "translate(" + (width/2) + " ," + 
-        (height + margin.top + 30) + ")")
+        (height + margin.top + 25) + ")")
     .style("text-anchor", "middle")
     .text("Date");
 
