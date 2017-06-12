54b46dcc3ea58cc04c07d5b9610c7a808ea1ce5a
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-12 10:56:18 -0500

    transcript search returns correct queryset

--- Prometheus/views/search_view.py
+++ Prometheus/views/search_view.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render
+from django.apps import apps
+import datetime
 
 
 def search (request):
-    print(request.path)
-    template = 'index.html'
     try:
         transcript = request.GET['transcript']
         corporation = transcript.split("-")[0]
+        transcript_date = transcript.split("-")[1]
+
+        transcript_date_for_db = datetime.datetime.strptime((transcript_date), "%d %B %y")
+        transcript_date_for_db = datetime.datetime.strftime((transcript_date_for_db), "%Y-%m-%d")
+
+        model = apps.get_model('Prometheus', corporation)
+
+        answers_query_set = model.objects.filter(date_of_call = transcript_date_for_db, question=0)
+        print("transcript_date_for_db", transcript_date_for_db)
+        print("answers_query_set", answers_query_set)
+        print("length", len(answers_query_set))
+
+        for answer in answers_query_set:
+            print(answer.name)
+
     except KeyError:
         corporation = ""
         pass
 
+    template = 'index.html'
     context = {"search_view": True, "corporation": corporation}
     return render(request, template, context)
