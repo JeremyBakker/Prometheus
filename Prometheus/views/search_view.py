b664058e22f50780a77b74a33691ba3a6e4208eb
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-12 09:52:42 -0500

    styled elements to fit one page

--- Prometheus/views/search_view.py
+++ Prometheus/views/search_view.py
@@ -4,13 +4,12 @@ from django.shortcuts import render
 def search (request):
     print(request.path)
     template = 'index.html'
+    try:
+        transcript = request.GET['transcript']
+        corporation = transcript.split("-")[0]
+    except KeyError:
+        corporation = ""
+        pass
 
-    transcript = request.GET['transcript']
-    corporation = transcript.split("-")[0]
-    print("transcript corporation", corporation)
-    print("type", type(corporation))
-
-    context = {"test": "TESSSSSSSSST", "corporation": corporation}
-    print("transcript", transcript)
-
+    context = {"search_view": True, "corporation": corporation}
     return render(request, template, context)
