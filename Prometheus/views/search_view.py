4ec18ea242bbe3c79850c4d44a0d7d01a6ed177f
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-14 12:09:42 -0500

    basic positive emotion works

--- Prometheus/views/search_view.py
+++ Prometheus/views/search_view.py
@@ -3,12 +3,10 @@ from django.apps import apps
 from nltk import word_tokenize
 from statistics import median
 import datetime
-import re
 import nltk
-import mmap
+import re
 
 def search (request):
-
 
     c_executive_o_list = ['^(Steve)? ?Jobs', '^(Tim)?(othy)? ?Cook']
     c_financial_o_list = ['^(Peter)? ?Oppenheimer', '^(Luca)? ?Maestri']
@@ -32,43 +30,46 @@ def search (request):
         c_financial_o_answer_list = list()
         c_executive_o_answer_length_list = list()
         c_financial_o_answer_length_list = list()
-        number_of_c_financial_o_negative_words = 0
         number_of_c_executive_o_negative_words = 0
-
+        number_of_c_financial_o_negative_words = 0
+        number_of_c_executive_o_positive_words = 0
+        number_of_c_financial_o_positive_words = 0
 
         for answer in answers_query_set:
             if c_executive_o_list_regex.search(answer.name):                
-                tokenized_answer = word_tokenize(answer.question_answer_text)
-                without_punctuation = re.compile('.*[A-Za-z0-9].*')
-                
-                filtered_answer = [word.upper() for word in tokenized_answer if without_punctuation.match(word)]
-
+                filtered_answer = clean_text(answer.question_answer_text)
                 c_executive_o_answer_list.append(filtered_answer)
                 c_executive_o_answer_length_list.append(len(filtered_answer))    
                 
                 with open('Prometheus/static/lexicons/negative_words.txt', 'r') as file:
                     lines = [line.strip() for line in file.readlines()]
                     if set(filtered_answer).intersection(lines):
-                        negative_intersection = set(filtered_answer).intersection(lines)
-                        negative_intersection_length = len(negative_intersection)
+                        negative_intersection_length = find_intersection_length(filtered_answer, lines)
                         number_of_c_executive_o_negative_words += negative_intersection_length
             
+                with open('Prometheus/static/lexicons/positive_words.txt', 'r') as file:
+                    lines = [line.strip() for line in file.readlines()]
+                    if set(filtered_answer).intersection(lines):
+                        positive_intersection_length = find_intersection_length(filtered_answer, lines)
+                        number_of_c_executive_o_positive_words += positive_intersection_length
+
             if c_financial_o_list_regex.search(answer.name):
-                answer = word_tokenize(answer.question_answer_text)
-                without_punctuation = re.compile('.*[A-Za-z0-9].*')
-                
-                filtered_answer = [word.upper() for word in answer if without_punctuation.match(word)]
-                
+                filtered_answer = clean_text(answer.question_answer_text)
+
                 c_financial_o_answer_list.append(filtered_answer)
                 c_financial_o_answer_length_list.append(len(filtered_answer))
                 
                 with open('Prometheus/static/lexicons/negative_words.txt', 'r') as file:
                     lines = [line.strip() for line in file.readlines()]
                     if set(filtered_answer).intersection(lines):
-                        print("CFO SET TRUE")
-                        negative_intersection = set(filtered_answer).intersection(lines)
-                        negative_intersection_length = len(negative_intersection)
+                        negative_intersection_length = find_intersection_length(filtered_answer, lines)
                         number_of_c_financial_o_negative_words += negative_intersection_length
+
+                with open('Prometheus/static/lexicons/positive_words.txt', 'r') as file:
+                    lines = [line.strip() for line in file.readlines()]
+                    if set(filtered_answer).intersection(lines):
+                        positive_intersection_length = find_intersection_length(filtered_answer, lines)
+                        number_of_c_financial_o_positive_words += positive_intersection_length
 
         c_executive_o_answer_length_sum = sum(c_executive_o_answer_length_list)
         c_financial_o_answer_length_sum = sum(c_financial_o_answer_length_list)
@@ -85,8 +86,11 @@ def search (request):
         corporation = ""
         pass
 
-    c_financial_o_negative_proportion = number_of_c_financial_o_negative_words / c_financial_o_answer_length_sum
     c_executive_o_negative_proportion = number_of_c_executive_o_negative_words / c_executive_o_answer_length_sum
+    c_financial_o_negative_proportion = number_of_c_financial_o_negative_words / c_financial_o_answer_length_sum
+    c_executive_o_positive_proportion = number_of_c_executive_o_positive_words / c_executive_o_answer_length_sum
+    c_financial_o_positive_proportion = number_of_c_financial_o_positive_words / c_financial_o_answer_length_sum
+    
 
     template = 'index.html'
     context = {
@@ -97,7 +101,20 @@ def search (request):
         "cFo_sum": c_financial_o_answer_length_sum,
         "cEo_median": c_executive_o_answer_length_median,
         "cEo_sum": c_executive_o_answer_length_sum,
-        "cFo_negative": c_financial_o_negative_proportion,
-        "cEo_negative": c_executive_o_negative_proportion
+        "cFo_negative": round(c_financial_o_negative_proportion, 4),
+        "cEo_negative": round(c_executive_o_negative_proportion, 4),
+        "cEo_positive": round(c_executive_o_positive_proportion, 4),
+        "cFo_positive": round(c_financial_o_positive_proportion, 4)
         }
     return render(request, template, context)
+
+def clean_text(question_answer_text):                
+    tokenized_answer = word_tokenize(question_answer_text)
+    without_punctuation = re.compile('.*[A-Za-z0-9].*')
+    filtered_answer = [word.upper() for word in tokenized_answer if without_punctuation.match(word)]
+    return filtered_answer
+
+def find_intersection_length(filtered_answer, lines):
+    intersection = set(filtered_answer).intersection(lines)
+    intersection_length = len(intersection)
+    return intersection_length
