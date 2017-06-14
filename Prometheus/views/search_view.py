9838cce742baa96e07b625cb72bd535cc88bb731
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-14 11:09:41 -0500

    basic negative emotions works

--- Prometheus/views/search_view.py
+++ Prometheus/views/search_view.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.apps import apps
+from nltk import word_tokenize
+from statistics import median
 import datetime
 import re
 import nltk
-from nltk import word_tokenize
-from statistics import median
-
+import mmap
 
 def search (request):
 
@@ -32,20 +32,43 @@ def search (request):
         c_financial_o_answer_list = list()
         c_executive_o_answer_length_list = list()
         c_financial_o_answer_length_list = list()
+        number_of_c_financial_o_negative_words = 0
+        number_of_c_executive_o_negative_words = 0
+
 
         for answer in answers_query_set:
             if c_executive_o_list_regex.search(answer.name):                
                 tokenized_answer = word_tokenize(answer.question_answer_text)
                 without_punctuation = re.compile('.*[A-Za-z0-9].*')
-                filtered_answer = [word for word in tokenized_answer if without_punctuation.match(word)]
+                
+                filtered_answer = [word.upper() for word in tokenized_answer if without_punctuation.match(word)]
+
                 c_executive_o_answer_list.append(filtered_answer)
-                c_executive_o_answer_length_list.append(len(filtered_answer))
+                c_executive_o_answer_length_list.append(len(filtered_answer))    
+                
+                with open('Prometheus/static/lexicons/negative_words.txt', 'r') as file:
+                    lines = [line.strip() for line in file.readlines()]
+                    if set(filtered_answer).intersection(lines):
+                        negative_intersection = set(filtered_answer).intersection(lines)
+                        negative_intersection_length = len(negative_intersection)
+                        number_of_c_executive_o_negative_words += negative_intersection_length
+            
             if c_financial_o_list_regex.search(answer.name):
                 answer = word_tokenize(answer.question_answer_text)
                 without_punctuation = re.compile('.*[A-Za-z0-9].*')
-                filtered_answer = [word for word in answer if without_punctuation.match(word)]
+                
+                filtered_answer = [word.upper() for word in answer if without_punctuation.match(word)]
+                
                 c_financial_o_answer_list.append(filtered_answer)
                 c_financial_o_answer_length_list.append(len(filtered_answer))
+                
+                with open('Prometheus/static/lexicons/negative_words.txt', 'r') as file:
+                    lines = [line.strip() for line in file.readlines()]
+                    if set(filtered_answer).intersection(lines):
+                        print("CFO SET TRUE")
+                        negative_intersection = set(filtered_answer).intersection(lines)
+                        negative_intersection_length = len(negative_intersection)
+                        number_of_c_financial_o_negative_words += negative_intersection_length
 
         c_executive_o_answer_length_sum = sum(c_executive_o_answer_length_list)
         c_financial_o_answer_length_sum = sum(c_financial_o_answer_length_list)
@@ -62,6 +85,8 @@ def search (request):
         corporation = ""
         pass
 
+    c_financial_o_negative_proportion = number_of_c_financial_o_negative_words / c_financial_o_answer_length_sum
+    c_executive_o_negative_proportion = number_of_c_executive_o_negative_words / c_executive_o_answer_length_sum
 
     template = 'index.html'
     context = {
@@ -71,6 +96,8 @@ def search (request):
         "cFo_median": c_financial_o_answer_length_median, 
         "cFo_sum": c_financial_o_answer_length_sum,
         "cEo_median": c_executive_o_answer_length_median,
-        "cEo_sum": c_executive_o_answer_length_sum
+        "cEo_sum": c_executive_o_answer_length_sum,
+        "cFo_negative": c_financial_o_negative_proportion,
+        "cEo_negative": c_executive_o_negative_proportion
         }
     return render(request, template, context)
