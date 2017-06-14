f538051fd745ef670ef28d0760df8ea7cce7075f
JeremyBakker <jeremy.t.bakker@gmail.com>
2017-06-13 20:46:30 -0500

    structure complete for cEo and cFo word counts and medians

--- Prometheus/views/search_view.py
+++ Prometheus/views/search_view.py
@@ -10,7 +10,7 @@ from statistics import median
 def search (request):
 
 
-    c_executive_o_list = ['^(Steve)? ?Jobs']
+    c_executive_o_list = ['^(Steve)? ?Jobs', '^(Tim)?(othy)? ?Cook']
     c_financial_o_list = ['^(Peter)? ?Oppenheimer', '^(Luca)? ?Maestri']
     c_executive_o_list_regex = re.compile(r'\b(?:%s)\b' % '|'.join(c_executive_o_list))
     c_financial_o_list_regex = re.compile(r'\b(?:%s)\b' % '|'.join(c_financial_o_list))
@@ -34,8 +34,12 @@ def search (request):
         c_financial_o_answer_length_list = list()
 
         for answer in answers_query_set:
-            if any(answer.name in name for name in c_executive_o_list):
-                pass
+            if c_executive_o_list_regex.search(answer.name):                
+                tokenized_answer = word_tokenize(answer.question_answer_text)
+                without_punctuation = re.compile('.*[A-Za-z0-9].*')
+                filtered_answer = [word for word in tokenized_answer if without_punctuation.match(word)]
+                c_executive_o_answer_list.append(filtered_answer)
+                c_executive_o_answer_length_list.append(len(filtered_answer))
             if c_financial_o_list_regex.search(answer.name):
                 answer = word_tokenize(answer.question_answer_text)
                 without_punctuation = re.compile('.*[A-Za-z0-9].*')
@@ -54,7 +58,6 @@ def search (request):
             c_financial_o_answer_length_median = int(median(c_financial_o_answer_length_list))
         except ValueError:
             c_financial_o_answer_length_median = "N/A"  
-
     except KeyError:
         corporation = ""
         pass
@@ -67,7 +70,7 @@ def search (request):
         "date": transcript_date_for_display, 
         "cFo_median": c_financial_o_answer_length_median, 
         "cFo_sum": c_financial_o_answer_length_sum,
-        "cEo_median": c_financial_o_answer_length_median,
+        "cEo_median": c_executive_o_answer_length_median,
         "cEo_sum": c_executive_o_answer_length_sum
         }
     return render(request, template, context)
